b5df76b7dfb9a6f464155bd5546aa632450b13b3
HuuHoai <daohuuhoai2655@gmail.com>
2024-03-18 23:33:01 +0700

    feat(create-user): done

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const {getFullUsers, creatUser} = require("../services/CRUD.service")
 const router = express.Router();
 
 const { getHomePage } = require("../controllers/homepage");
 
+
+
 router.get("/", getHomePage)
+router.post("/creating-user", creatUser)
 
 module.exports = router;
--- src/sever.js
+++ src/sever.js
@@ -7,6 +7,9 @@ const configViewEngine = require("./config/viewEngine");
 const port = process.env.PORT;
 const webRoute = require("./routes/web");
 
+app.use(express.json()); // Used to parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+
 
 configViewEngine(app);
 
