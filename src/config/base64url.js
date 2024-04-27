d62d27bf6578b988d4786cf662286bbee31c4c64
HuuHoai <daohuuhoai2655@gmail.com>
2024-04-27 13:19:49 +0700

    add: add prettier

--- src/config/base64url.js
+++ src/config/base64url.js
@@ -1,5 +1,5 @@
-const base64url = (str) => {
-  return btoa(str).replace(/\+/g, "-").replace(/\//g, "-").replace(/\=/g, "");
-};
+const base64url = str => {
+    return btoa(str).replace(/\+/g, '-').replace(/\//g, '-').replace(/\=/g, '')
+}
 
-module.exports = base64url;
+module.exports = base64url
--- src/middlewares/clientUserInfor.js
+++ src/middlewares/clientUserInfor.js
@@ -1,19 +1,19 @@
-const { queryGetAllUserInfo } = require("./queryDB");
+const { queryGetAllUserInfo } = require('./queryDB')
 class ClientUserInfor {
-  getUserInforFromClient = (req) => {
-    let email = req.body.email;
-    let userName = req.body.username;
-    let password = req.body.password;
-    return {
-      email: `${email}`,
-      userName: `${userName}`,
-      password: `${password}`,
-    };
-  };
+    getUserInforFromClient = req => {
+        let email = req.body.email
+        let userName = req.body.username
+        let password = req.body.password
+        return {
+            email: `${email}`,
+            userName: `${userName}`,
+            password: `${password}`,
+        }
+    }
 
-  dbGetFullUsers = async () => {
-    return queryGetAllUserInfo();
-  };
+    dbGetFullUsers = async () => {
+        return queryGetAllUserInfo()
+    }
 }
 
-module.exports = new ClientUserInfor();
+module.exports = new ClientUserInfor()
